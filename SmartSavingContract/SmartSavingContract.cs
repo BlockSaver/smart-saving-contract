c326b392c3694d1c0da9e856d2e55c12e39b0f06
Marin Petrunić <marin.petrunic@gmail.com>
2017-11-12 02:24:20 +0100

    Initial contract version

---
+++ SmartSavingContract/SmartSavingContract.cs
@@ -0,0 +1,38 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Numerics;
+
+namespace SmartSavingContract
+{
+    public class SmartSavingContract : SmartContract
+    {
+        public static object Main(string operation, params object[] args)
+        {
+            switch (operation)
+            {
+                case "createSavings":
+                    {
+                        return "savingsId";
+                    }
+                case "getAllSavings":
+                    {
+                        return "[]";
+                    }
+                case "getSavings":
+                    {
+                        return null;
+                    }
+                case "transfer":
+                    {
+                        return false;
+                    }
+                case "closeSavings":
+                    {
+                        return false;
+                    }
+            }
+            return "unknown command";
+        }
+    }
+}
